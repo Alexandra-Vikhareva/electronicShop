79c125198274a3e21985ca2f6f536d1fb06ad4fb
Sasha <st087955@student.spbu.ru>
2026-05-06 21:40:27 +0300

    Настроена маршрутизация для всех элементов

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Home from './pages/Home/Home.jsx'
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import Shop from './pages/Shop/Shop.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
-  }
+    element: <Home/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "/cart",
+    element: <Cart/>,
+  },
+  {
+    path: "/shop",
+    element: <Shop/>,
+  },
 ])
 
 createRoot(document.getElementById('root')).render(
