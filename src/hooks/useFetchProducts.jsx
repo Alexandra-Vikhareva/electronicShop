1b98a092ecaeef7d8170d5606a8ba385d43e5530
Sasha <st087955@student.spbu.ru>
2026-05-07 14:26:08 +0300

    Создан пользовательский хук, который получает данные из API

---
+++ src/hooks/useFetchProducts.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+  
+export function useFetchProducts() {
+    const [productsList, setProductsList] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    fetch('https://fake-store-api.mock.beeceptor.com/api/products')
+    .then((response) => {
+        if (response.status >= 400) {
+            throw new Error('server error');
+        }
+        return response.json()
+    })
+    .then((response) => setProductsList(response))
+    .catch((error) => setError(error))
+    .finally(() => setLoading(false))
+    }, []);
+
+    return {productsList, error, loading};
+};
