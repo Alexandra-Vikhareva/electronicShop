3317810e73f53ccb74065d2efa89cecb37caf30f
Sasha <st087955@student.spbu.ru>
2026-05-07 15:58:03 +0300

    Создана карточка товара с мок данными

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,21 @@
+import styles from './ProductCard.module.css';
+
 export default function ProductCard() {
     return (
-        <div className="product-card">
-            <img src="src/assets/images/headphones.png" alt="headphones image" />
-            <div className="info">
-                <div className="col1">
-                    <div className="itemName">Apple BYZ S8521</div>
-                    <div className="star">
-                        <img src="src/assets/images/Star.svg" alt="star icon" className="icon" />
-                        <span className="rating">4.7</span>
+        <div className={styles.productCard}>
+            <img src="src/assets/images/headphones.png" alt="headphones image" className={styles.photo}/>
+            <div className={styles.info}>
+                <div className={styles.col1}>
+                    <div className={styles.itemName}>Apple BYZ S8521</div>
+                    <div className={styles.star}>
+                        <img src="src/assets/images/Star.svg" alt="star icon" className={styles.icon} />
+                        <span className={styles.rating}>4.7</span>
                     </div>
                 </div>
 
-                <div className="col2">
-                    <div className="price">2927 P</div>
-                    <button className="btn_buy">Купить</button>
+                <div className={styles.col2}>
+                    <div className={styles.price}>2927 P</div>
+                    <button className={styles.btn_buy}>Купить</button>
                 </div>
             </div>
         </div>
