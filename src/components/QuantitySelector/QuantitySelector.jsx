d1c703550970dd8a409fcc96bd96e63237831fc2
Sasha <st087955@student.spbu.ru>
2026-05-15 17:53:02 +0300

    Убраны лишние выводы для дебагинга

--- src/components/QuantitySelector/QuantitySelector.jsx
+++ src/components/QuantitySelector/QuantitySelector.jsx
@@ -41,16 +41,6 @@ export default function QuantitySelector({quantity, setQuantity, addToCart, prod
         }
     }
 
-    // function handleChangeCart(e) {
-    //     let value = Number(e.target.value);        
-    //     addToCart({
-    //             product: product.id, 
-    //             title: product.title,
-    //             price: product.price,
-    //             image: product.image,
-    //             quantity: value});
-    // }
-
     return (
         <div className={styles.quantitySelector}>
             <button onClick={handleMinusButton} 
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
 import { useFetchProducts } from "../../hooks/useFetchProducts";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import styles from './Shop.module.css'
 
 export default function Shop() {
     const {productsList, error, loading} = useFetchProducts();
-
-    useEffect(() => {
-        const a = productsList;
-        console.log(a)
-    }, [productsList]);
 
     if (error) return <p>Error</p>;
     if(loading) return <p>Loading...</p>;
